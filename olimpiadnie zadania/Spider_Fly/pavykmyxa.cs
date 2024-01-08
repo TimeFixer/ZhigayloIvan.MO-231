ed19ab22fb882837f1a5b575bdc3e45ddb10c7e1
TimeFixer <144201559+TimeFixer@users.noreply.github.com>
2024-01-08 14:02:12 +0600

    olimpiad

---
+++ olimpiadnie zadania/Spider_Fly/pavykmyxa.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace muha
+{
+    internal class Program
+    {
+        const int X = 0;
+        const int Y = 1;
+        const int Z = 2;
+        static void Main(string[] args)
+        {
+            string path = "\\input.txt"; //Расположение файла
+            StreamReader reader = new StreamReader(path);
+            string state = "none";
+            bool sw = false;
+            double answer = 0;
+            int[] size = new int[3];
+            int[] spider = new int[3];
+            int[] fly = new int[3];
+            size = Array.ConvertAll(reader.ReadLine().Split(' ').ToArray(), int.Parse);
+            spider = Array.ConvertAll(reader.ReadLine().Split(' ').ToArray(), int.Parse);
+            fly = Array.ConvertAll(reader.ReadLine().Split(' ').ToArray(), int.Parse);
+
+            for (int i = 0; i < 3; i++) {
+                if ((spider[i] == 0 && fly[i] == 0) || (spider[i] == size[i] && fly[i] == size[i])){
+                    state = "easy";
+                    break;
+                }
+                else if ((spider[i] == 0 && fly[i] == size[i]) || (spider[i] == size[i] && fly[i] == 0)){
+                    state = "hard";
+                }
+                if (spider[i] == fly[i] && spider[i] != 0 && spider[i] != size[i]){
+                    sw = true;
+                }
+            }
+            if (state == "none"){
+                state = "normal";
+            }
+            if (state == "easy"){
+                if (sw){
+                    answer = Math.Abs(spider[X] - fly[X]) + Math.Abs(spider[Y] - fly[Y]) + Math.Abs(spider[Z] - fly[Z]);
+                }
+                else{
+                    answer = Math.Sqrt(Math.Pow(Math.Abs(spider[X] - fly[X]), 2) + Math.Pow(Math.Abs(spider[Y] - fly[Y]), 2) + Math.Pow(Math.Abs(spider[Z] - fly[Z]), 2));
+                }
+            }
+            else if (state == "normal"){
+                if (sw){
+                    answer = Math.Abs(spider[X] - fly[X]) + Math.Abs(spider[Y] - fly[Y]) + Math.Abs(spider[Z] - fly[Z]);
+                }
+                else{
+                    int spiderSide = -1;
+                    int flySide = -1;
+                    int otherSide = -1;
+                    for (int i = 0; i < 3; i++){
+                        if ((spider[i] == 0 || spider[i] == size[i]) && spiderSide == -1){
+                            spiderSide = i;
+                        }
+                        else if ((fly[i] == 0 || fly[i] == size[i]) && flySide == -1){
+                            flySide = i;
+                        }
+                        else{
+                            otherSide = i;
+                        }
+                    }
+                    answer = Math.Sqrt(Math.Pow(Math.Abs(spider[flySide] - fly[flySide]) + Math.Abs(spider[spiderSide] - fly[spiderSide]), 2) + Math.Pow(Math.Abs(spider[otherSide] - fly[otherSide]), 2));
+                }
+            }
+            else if (state == "hard"){
+                bool just_HARDLine = false;
+                int common_Side = -1;
+                int[] other_Side = new int[2];
+                for (int i = 0, n = 0; i < 3; i++){
+                    if (((spider[i] == 0 && fly[i] == size[i]) || (spider[i] == size[i] && fly[i] == 0)) && common_Side == -1){
+                        common_Side = i;
+                    }
+                    else {
+                        other_Side[n++] = i;
+                    }
+                }
+                if ( spider[other_Side[0]] == fly[other_Side[0]] && spider[other_Side[1]] == fly[other_Side[1]]) just_HARDLine = true;
+
+                double[] paths = new double[4];
+                paths[0] = size[common_Side] + (size[other_Side[0]] - spider[other_Side[0]]) + (size[other_Side[0]] - fly[other_Side[0]]);
+                paths[1] = size[common_Side] + spider[other_Side[0]] + fly[other_Side[0]];
+                paths[2] = size[common_Side] + (size[other_Side[1]] - spider[other_Side[1]]) + (size[other_Side[1]] - fly[other_Side[1]]);
+                paths[3] = size[common_Side] + spider[other_Side[1]] + fly[other_Side[1]];
+
+                if (just_HARDLine){
+                    answer = paths[0];
+                    for (int i = 1; i < 4; i++) {answer = Math.Min(answer, paths[i]);}
+                }
+                else{
+                    paths[0] = Math.Sqrt(Math.Pow(paths[0], 2) + Math.Pow(Math.Abs(spider[other_Side[1]] - fly[other_Side[1]]), 2));
+                    paths[1] = Math.Sqrt(Math.Pow(paths[1], 2) + Math.Pow(Math.Abs(spider[other_Side[1]] - fly[other_Side[1]]), 2));
+                    paths[2] = Math.Sqrt(Math.Pow(paths[2], 2) + Math.Pow(Math.Abs(spider[other_Side[0]] - fly[other_Side[0]]), 2));
+                    paths[3] = Math.Sqrt(Math.Pow(paths[3], 2) + Math.Pow(Math.Abs(spider[other_Side[0]] - fly[other_Side[0]]), 2));
+                    answer = paths[0];
+                    for (int i = 1; i < 4; i++){answer = Math.Min(answer, paths[i]);}
+                }
+            }
+            Console.WriteLine(Math.Round(answer, 3));
+        }
+    }
+}
---
+++ olimpiadnie zadania/denegobmen/obmen.cs
@@ -0,0 +1,74 @@
+﻿
+class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("input.txt");
+            int[] rate1; int[] rate2; int[] factor1; int[] factor2; int[] money2;
+
+            try{
+                string[] kurs01 = reader.ReadLine().Split(" ", 2)[1].Split(" "); rate1 = new int[kurs01.Length];
+                for (int i = 0; i < kurs01.Length; i++){
+                    rate1[i] = Convert.ToInt32(kurs01[i]);
+                }
+            } catch { rate1 = new int[1]; }
+            try{
+                string[] lucky01 = reader.ReadLine().Split(" ", 2)[1].Split(" "); factor1 = new int[lucky01.Length];
+                for (int i = 0; i < lucky01.Length; i++){
+                    factor1[i] = Convert.ToInt32(lucky01[i]);
+                }
+            } catch { factor1 = new int[1]; }
+            try{
+                string[] kurs02 = reader.ReadLine().Split(" ", 2)[1].Split(" "); rate2 = new int[kurs02.Length];
+                for (int i = 0; i < kurs02.Length; i++)
+                {
+                    rate2[i] = Convert.ToInt32(kurs02[i]);
+                }
+            } catch { rate2 = new int[1]; }
+            try{
+                string[] lucky02 = reader.ReadLine().Split(" ", 2)[1].Split(" "); factor2 = new int[lucky02.Length];
+                for (int i = 0; i < lucky02.Length; i++)
+                {
+                    factor2[i] = Convert.ToInt32(lucky02[i]);
+                }
+            } catch { factor2 = new int[1]; }
+            string[] money01= reader.ReadLine().Split(" "); int[] money1 = new int[money01.Length];
+            for (int i = 0; i < money1.Length; i++){
+                money1[i]= Convert.ToInt32(money01[i]);
+            }
+            if (rate2[0] != 0) {  money2 = new int[rate2.Length + 1]; } else { money2 = new int[1]; } 
+
+
+            Array.Sort(factor1); Array.Sort(factor2);Array.Reverse(factor1);
+
+            for (int i = 0; i < money1.Length; i++){
+                for(int t=0; t<factor1.Length; t++){
+                    if (factor1[t] <= money1[i])
+                    {
+                        money1[i]--;
+                    }
+                }
+            }
+            for (int i = 0; i < money1.Length - 1; i++){
+                money1[i + 1] += money1[i] * rate1[i];
+            }
+            money2[money2.Length-1] = money1[money1.Length-1];
+
+            for (int i = money2.Length-1;i>0; i--){
+
+                money2[i - 1] = money2[i] / rate2[i-1];
+                money2[i] = money2[i] % rate2[i - 1];
+            }
+
+            for (int i = 0; i < money2.Length; i++){
+                for (int t = 0; t < factor2.Length; t++){
+                    if (factor2[t] <= money2[i]){
+                        money2[i]++;
+                    }
+                }
+            }
+            for (int i = 0;i < money2.Length; i++){
+                Console.Write("{0} ", money2[i]);
+            }
+        }
+    }
