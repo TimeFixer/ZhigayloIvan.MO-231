3d46fe4e4a13c719f2d53575c0799a55018817de
TimeFixer <144201559+TimeFixer@users.noreply.github.com>
2024-01-11 13:47:09 +0600

    Add files via upload

---
+++ algoritmisatsia/lab14/kratnost3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int min1, min2, answer, n1, n2, razn, ost, m1, m2;
+        min1 = min2 = 10000;
+        answer = 0;
+        for (int i = 0; i < n; i++)
+        {
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            answer += Math.Max(n1, n2);
+            razn = Math.Abs(n1 - n2);
+            ost = razn % 3;
+            if (ost > 0)
+            {
+                m1 = min1; m2 = min2;
+
+                m1 = Math.Min(m1, Math.Min((razn + min1) % 3 == 1 ? razn + min1 : 1000, (razn + min2) % 3 == 1 ? razn + min2 : 1000));
+                m2 = Math.Min(m2, Math.Min((razn + min1) % 3 == 2 ? razn + min1 : 1000, (razn + min2) % 3 == 2 ? razn + min2 : 1000));
+
+                if (ost == 1) 
+                { 
+                    m1 = Math.Min(m1, razn); 
+                }
+                else { m2 = Math.Min(m2, razn); }
+                min1 = m1; min2 = m2;
+            }
+        }
+        if (answer % 3 == 0) 
+        { 
+            Console.WriteLine(answer); 
+        }
+        else if ((answer % 3 == 1) && (Math.Abs(answer - min1) % 3 == 0) && (answer - min1) > 0) 
+        { 
+            Console.WriteLine(answer - min1); 
+        }
+        else if ((answer % 3 == 2) && (Math.Abs(answer - min2) % 3 == 0) && (answer - min2) > 0) 
+        { 
+            Console.WriteLine(answer - min2); 
+        }
+        else 
+        {
+            Console.WriteLine("Не существует"); 
+        }
+    }
+}
---
+++ algoritmisatsia/lab15/trassa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество участков трассы");
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите время прохождения каждого");
+        int t = int.Parse(Console.ReadLine());
+        Console.WriteLine("введите время прохождений участков на a и b используя пробел");
+        int[] mass_a = new int[N];
+        int[] mass_b = new int[N];
+        int l = 0; int l1 = 0;
+        for (int i = 0; i < N; i++)
+        {
+            string[] S = Console.ReadLine().Split( );
+            int a = int.Parse(S[0]);
+            int b = int.Parse(S[1]);
+            mass_a[i] = a + l;
+            mass_b[i] = b + l1;
+            l = mass_a[i];
+            l1 = mass_b[i];
+        }
+        int ans = t + mass_b[mass_b.Length - 1];
+        for (int i = 0; i < mass_b.Length; i++)
+        {
+            if (mass_a[i] + t + (mass_b[N - 1] - mass_b[i]) < ans)
+            {
+                ans = mass_a[i] + t + (mass_b[N - 1] - mass_b[i]);
+            }
+        }
+        Console.WriteLine("Наименьшее потраченное время: " + ans);
+    }
+}
