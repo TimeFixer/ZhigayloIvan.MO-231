709fa39463d7917ad4c1e8f66f83f54acf3c4a19
TimeFixer <144201559+TimeFixer@users.noreply.github.com>
2024-05-15 10:33:45 +0100

    Add files via upload

---
+++ DiscretMatch/floida.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ConsoleApp53
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph =    {{0,4,0,0,3,5,0},
+                               {0,0,0,0,4,4,0},
+                               {0,1,0,3,0,3,0},
+                               {0,0,0,0,0,0,2},
+                               {0,0,6,0,0,0,0},
+                               {0,0,0,3,0,0,0},
+                               {0,0,0,0,0,0,2}};
+            for (int k = 0; k < graph.GetLength(0); k++)
+            {
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    for (int j = 0; j < graph.GetLength(0); j++)
+                        if (i != j && graph[k, j] != 0 && graph[i, k] != 0 && (graph[i, j] > graph[i, k] + graph[k, j] || graph[i, j] == 0))
+                        {
+                            graph[i, j] = graph[i, k] + graph[k, j];
+                        }
+                }
+            }
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                for (int j = 0; j < graph.GetLength(0); j++) Console.Write(graph[i, j] + "  ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
